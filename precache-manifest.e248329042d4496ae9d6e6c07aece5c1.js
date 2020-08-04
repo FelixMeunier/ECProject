809885f2dd777db00e228b6cf7a95f289bf1bdc7
FelixMeunier <fmeun013@uottawa.ca>
2020-08-04 16:46:55 -0400

    Updates

---
+++ precache-manifest.e248329042d4496ae9d6e6c07aece5c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "385cd99d101f5a9439c8dc78c3513023",
+    "url": "/ECProject/index.html"
+  },
+  {
+    "revision": "dcf2b40c104f4e618135",
+    "url": "/ECProject/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b3c6801ab28d674de675",
+    "url": "/ECProject/static/js/2.502aae9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ECProject/static/js/2.502aae9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcf2b40c104f4e618135",
+    "url": "/ECProject/static/js/main.f5eb0de9.chunk.js"
+  },
+  {
+    "revision": "87c8acaa7ce95b19de20",
+    "url": "/ECProject/static/js/runtime-main.098458bd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ECProject/static/media/logo.5d5d9eef.svg"
+  }
+]);
